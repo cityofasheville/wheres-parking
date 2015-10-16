1340282cbc298dbea45116666c8ccb8facb3c966
carlyleec <carlyleec@gmail.com>
2015-10-16 16:58:40 -0400

    setInterval to refresh every 10secs

--- app.js
+++ app.js
@@ -2,40 +2,42 @@
 var app = angular.module('app', []);
  
 //Configure application states and routes
-app.config(function () {
-    
-    // $urlRouterProvider.when('/topics', '/topics/list');
-      
-  });//END config
+app.config(function () {});//END config
 
 app.controller('AppCtrl', ['$scope', '$http',
  function ($scope, $http) {
+
+    //Geolocation
     $scope.geolocationAllowed = true;
     $scope.usersPosition = "";
+
     var geolocationCallback = function(position){
         $scope.usersPosition = position.coords.latitude + "," + position.coords.longitude;
         $scope.$apply();
-        console.log($scope.usersPosition);
     }
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(geolocationCallback);
     } else {
         $scope.geolocationAllowed = false;
     }
+
+    //Parking deck spaces data
     $scope.decks = [];
-    $http({method : 'GET', url : 'https://s3.amazonaws.com/asheville-parking-decks/spaces.json'})
+
+    var getDataFromS3 = function(){
+      $http({method : 'GET', url : 'https://s3.amazonaws.com/asheville-parking-decks/spaces.json'})
           //callbacks
           .success(function(data, status, headers, config){
             $scope.decks = data.decks;
-            console.log($scope.decks);
           })
           .error(function(error){
               console.log('Error querying feature service.');
               console.log(error);
           });
-
-    $scope.onClickGetGoogleDirections = function(){
-
     }
+
+    getDataFromS3();
+
+    setInterval(getDataFromS3, 10000);
 
 }]);
