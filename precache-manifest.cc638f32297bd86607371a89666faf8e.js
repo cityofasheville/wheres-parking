8c77f99f52223124ed83dcf615eb62b14fafc8b7
Frances Ruiz <frances.ruiz@gmail.com>
2019-04-08 15:03:00 -0400

    Updates

---
+++ precache-manifest.cc638f32297bd86607371a89666faf8e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c1878c2227d688fcb36341f43347c8d",
+    "url": "/wheres-parking/static/media/git_hub.7c1878c2.svg"
+  },
+  {
+    "revision": "f8b7c0ba82461e087cdca2d082a58cf9",
+    "url": "/wheres-parking/static/media/direction_icon.f8b7c0ba.svg"
+  },
+  {
+    "revision": "d159fa53154d136b97426fb2221a0f74",
+    "url": "/wheres-parking/static/media/city_logo_no_text.d159fa53.svg"
+  },
+  {
+    "revision": "f75552e527f9b723d46d469697783e84",
+    "url": "/wheres-parking/static/media/city_logo.f75552e5.svg"
+  },
+  {
+    "revision": "5e26dfd1bf9ba73bbf78",
+    "url": "/wheres-parking/static/js/runtime~main.69d8db84.js"
+  },
+  {
+    "revision": "8f7140504459d3ab99be",
+    "url": "/wheres-parking/static/js/main.2a38a228.chunk.js"
+  },
+  {
+    "revision": "73c389ff5051919f0b63",
+    "url": "/wheres-parking/static/js/2.508582b8.chunk.js"
+  },
+  {
+    "revision": "8f7140504459d3ab99be",
+    "url": "/wheres-parking/static/css/main.247efc6b.chunk.css"
+  },
+  {
+    "revision": "0b8bd7f598a78af5256f30400f945ae6",
+    "url": "/wheres-parking/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,34 @@
-"use strict";var precacheConfig=[["/wheres-parking/index.html","5b485d024b6f1340253cfcdb987fdb6d"],["/wheres-parking/static/css/main.8cb6c8e0.css","999dc2dcbecf4b9815b47b60f709270c"],["/wheres-parking/static/js/main.3bc0d3f6.js","b31262049478916bf41c98879bdbd719"],["/wheres-parking/static/media/city_logo.b3aa6a63.svg","b3aa6a635f69549e88330ec4d9b91e98"],["/wheres-parking/static/media/city_logo_no_text.b75092d7.svg","b75092d7552fb0b0dd8165bed3672b84"],["/wheres-parking/static/media/direction_icon.93c7a940.svg","93c7a9405a6dd6ee31f0455b10030463"],["/wheres-parking/static/media/git_hub.7c1878c2.svg","7c1878c2227d688fcb36341f43347c8d"]],cacheName="sw-precache-v3-sw-precache-webpack-plugin-"+(self.registration?self.registration.scope:""),ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,t){var n=new URL(e);return"/"===n.pathname.slice(-1)&&(n.pathname+=t),n.toString()},cleanResponse=function(t){return t.redirected?("body"in t?Promise.resolve(t.body):t.blob()).then(function(e){return new Response(e,{headers:t.headers,status:t.status,statusText:t.statusText})}):Promise.resolve(t)},createCacheKey=function(e,t,n,r){var a=new URL(e);return r&&a.pathname.match(r)||(a.search+=(a.search?"&":"")+encodeURIComponent(t)+"="+encodeURIComponent(n)),a.toString()},isPathWhitelisted=function(e,t){if(0===e.length)return!0;var n=new URL(t).pathname;return e.some(function(e){return n.match(e)})},stripIgnoredUrlParameters=function(e,n){var t=new URL(e);return t.hash="",t.search=t.search.slice(1).split("&").map(function(e){return e.split("=")}).filter(function(t){return n.every(function(e){return!e.test(t[0])})}).map(function(e){return e.join("=")}).join("&"),t.toString()},hashParamName="_sw-precache",urlsToCacheKeys=new Map(precacheConfig.map(function(e){var t=e[0],n=e[1],r=new URL(t,self.location),a=createCacheKey(r,hashParamName,n,/\.\w{8}\./);return[r.toString(),a]}));function setOfCachedUrls(e){return e.keys().then(function(e){return e.map(function(e){return e.url})}).then(function(e){return new Set(e)})}self.addEventListener("install",function(e){e.waitUntil(caches.open(cacheName).then(function(r){return setOfCachedUrls(r).then(function(n){return Promise.all(Array.from(urlsToCacheKeys.values()).map(function(t){if(!n.has(t)){var e=new Request(t,{credentials:"same-origin"});return fetch(e).then(function(e){if(!e.ok)throw new Error("Request for "+t+" returned a response with status "+e.status);return cleanResponse(e).then(function(e){return r.put(t,e)})})}}))})}).then(function(){return self.skipWaiting()}))}),self.addEventListener("activate",function(e){var n=new Set(urlsToCacheKeys.values());e.waitUntil(caches.open(cacheName).then(function(t){return t.keys().then(function(e){return Promise.all(e.map(function(e){if(!n.has(e.url))return t.delete(e)}))})}).then(function(){return self.clients.claim()}))}),self.addEventListener("fetch",function(t){if("GET"===t.request.method){var e,n=stripIgnoredUrlParameters(t.request.url,ignoreUrlParametersMatching),r="index.html";(e=urlsToCacheKeys.has(n))||(n=addDirectoryIndex(n,r),e=urlsToCacheKeys.has(n));var a="/wheres-parking/index.html";!e&&"navigate"===t.request.mode&&isPathWhitelisted(["^(?!\\/__).*"],t.request.url)&&(n=new URL(a,self.location).toString(),e=urlsToCacheKeys.has(n)),e&&t.respondWith(caches.open(cacheName).then(function(e){return e.match(urlsToCacheKeys.get(n)).then(function(e){if(e)return e;throw Error("The cached response that was expected is missing.")})}).catch(function(e){return console.warn('Couldn\'t serve response for "%s" from cache: %O',t.request.url,e),fetch(t.request)}))}});
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/wheres-parking/precache-manifest.cc638f32297bd86607371a89666faf8e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/wheres-parking/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
