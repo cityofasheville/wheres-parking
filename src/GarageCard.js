7732cb331fea06b6567744ecf6d77de644a5936a
Thomson Knoles <thomsonknoles@gmail.com>
2024-02-12 13:22:07 -0500

    destructure props, accessibility, garage name truncation

--- src/GarageCard.js
+++ src/GarageCard.js
@@ -4,38 +4,45 @@ import city_logo_no_text from './city_logo_no_text.svg';
 
 const city_owned = ['Biltmore Ave.Garage', "Harrah's Cherokee Center Garage", 'Rankin Ave Garage', 'Wall Street Garage'];
 
-const GarageCard = props => (
-  <div>
-    {props.available === undefined || props.coords === undefined ?
-      <div>Loading...</div>
-      :
-      <a
-        className="GarageCard-card"
-        href={`https://maps.google.com/?saddr=Current+Location&daddr=${props.coords[0]},${props.coords[1]}`}
-        target="_blank" rel="noopener noreferrer"
-      >
-        <span className="GarageCard-name">
-          <img
-            src={city_owned.indexOf(props.name) > -1 ? city_logo_no_text : bc_logo_brand}
-            alt={city_owned.indexOf(props.name) > -1 ? 'City of Asheville icon' : 'Buncombe County icon'}
-            className={city_owned.indexOf(props.name) > -1 ? 'GarageCard-city-icon' : 'GarageCard-bc-icon'}
-          />
-          <span className="GarageCard-name-text">
-            { 
-              props.name.length > 3 && props.name.substring(props.name.length - 6) === 'Garage' ? props.name.substring(0, props.name.length - 7)
-              :
-              props.name
-            }
-            <span className="hidden">garage</span>
+const GarageCard = ({ available, coords, name }) => {
+  // Function to remove "Garage" from the name if applicable
+  const formatName = (garageName) => {
+    const garageSuffix = " Garage";
+    return garageName.endsWith(garageSuffix) ? garageName.slice(0, -garageSuffix.length) : garageName;
+  };
+
+  const isCityOwned = city_owned.includes(name);
+  const icon = isCityOwned ? city_logo_no_text : bc_logo_brand;
+  const altText = isCityOwned ? 'City of Asheville icon' : 'Buncombe County icon';
+  const iconNameClass = isCityOwned ? 'GarageCard-city-icon' : 'GarageCard-bc-icon';
+
+  return (
+    <div>
+      {available === undefined || coords === undefined ? (
+        <div>Loading...</div>
+      ) : (
+        <a
+          className="GarageCard-card"
+          href={`https://maps.google.com/?saddr=Current+Location&daddr=${coords[0]},${coords[1]}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label={`Directions to ${name}`}
+        >
+          <span className="GarageCard-name">
+            <img src={icon} alt={altText} className={iconNameClass} />
+            <span className="GarageCard-name-text">
+              {formatName(name)}
+              <span className="hidden">garage</span>
+            </span>
           </span>
-        </span>
-        <span className="GarageCard-available">
-          {props.available}
-          <span className="hidden">open spaces</span>
-        </span>
-      </a>
-    }
-  </div>
-);
+          <span className="GarageCard-available">
+            {available}
+            <span className="hidden">open spaces</span>
+          </span>
+        </a>
+      )}
+    </div>
+  );
+};
 
 export default GarageCard;
