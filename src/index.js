fb33870b7aa81bd238ca7ffad3447e580d40364d
alletizia <aletizia529@gmail.com>
2025-02-03 14:05:12 -0500

    update to node v22, react v18, and react-scripts v5

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+const container = document.getElementById('root');
+const root = createRoot(container); 
+root.render(<App />)
+registerServiceWorker();
