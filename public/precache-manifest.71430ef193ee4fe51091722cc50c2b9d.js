7439e746d378f006825ace6b23cc6003bbc4d6c3
alletizia <aletizia529@gmail.com>
2025-02-03 14:47:46 -0500

    update precache manifest to build file names

--- public/precache-manifest.71430ef193ee4fe51091722cc50c2b9d.js
+++ public/precache-manifest.71430ef193ee4fe51091722cc50c2b9d.js
@@ -5,38 +5,38 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "1a589334afaffc268f2b",
-    "url": "./static/css/main.054fc62d.chunk.css"
+    "url": "./static/css/main.a77ca4c4.css"
   },
   {
     "revision": "b117931e66eedde01ce5",
-    "url": "./static/js/2.740fe89a.chunk.js"
+    "url": "./static/js/main.7a75188f.js"
   },
   {
     "revision": "ad19c7eadd74fd1b468c2d31f8d83270",
-    "url": "./static/js/2.740fe89a.chunk.js.LICENSE.txt"
+    "url": "./static/js/main.7a75188f.js.LICENSE.txt"
   },
-  {
-    "revision": "1a589334afaffc268f2b",
-    "url": "./static/js/main.9d82ecf1.chunk.js"
-  },
-  {
-    "revision": "b5127706b999a43cb8af",
-    "url": "./static/js/runtime-main.c252ba72.js"
-  },
+  // {
+  //   "revision": "1a589334afaffc268f2b",
+  //   "url": "./static/js/main.9d82ecf1.chunk.js"
+  // },
+  // {
+  //   "revision": "b5127706b999a43cb8af",
+  //   "url": "./static/js/runtime-main.c252ba72.js"
+  // },
   {
     "revision": "b3aa6a635f69549e88330ec4d9b91e98",
-    "url": "./static/media/city_logo.b3aa6a63.svg"
+    "url": "./static/media/city_logo.d1e5dd3d86c1687e8864f62d20064429.svg"
   },
   {
     "revision": "b75092d7552fb0b0dd8165bed3672b84",
-    "url": "./static/media/city_logo_no_text.b75092d7.svg"
+    "url": "./static/media/city_logo_no_text.ef4c07c8ee6397ab6952a2b8a493e07d.svg"
   },
   {
     "revision": "93c7a9405a6dd6ee31f0455b10030463",
-    "url": "./static/media/direction_icon.93c7a940.svg"
+    "url": "./static/media/direction_icon.81be8bcafa9cd62d7e366002af86ef2a.svg"
   },
   {
     "revision": "7c1878c2227d688fcb36341f43347c8d",
-    "url": "./static/media/git_hub.7c1878c2.svg"
+    "url": "./static/media/git_hub.aebdc18ef12751c349823171b050b7c2.svg"
   }
 ]);
